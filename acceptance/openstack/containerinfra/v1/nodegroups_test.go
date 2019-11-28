2df22212deaa13a6509718df0cdfbe5d4172ec28
Thomas Hartland <thomas.george.hartland@cern.ch>
2019-11-28 10:07:13 +0100

    Move create kubernetes cluster to acceptance containerinfra.go

--- acceptance/openstack/containerinfra/v1/nodegroups_test.go
+++ acceptance/openstack/containerinfra/v1/nodegroups_test.go
@@ -3,15 +3,10 @@
 package v1
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/acceptance/clients"
-	"github.com/gophercloud/gophercloud/acceptance/tools"
-	"github.com/gophercloud/gophercloud/openstack/containerinfra/v1/clusters"
-	"github.com/gophercloud/gophercloud/openstack/containerinfra/v1/clustertemplates"
 	"github.com/gophercloud/gophercloud/openstack/containerinfra/v1/nodegroups"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
@@ -31,11 +26,11 @@ func TestNodeGroupsCRUD(t *testing.T) {
 
 	client.Microversion = "1.9"
 
-	clusterTemplate, err := createKubernetesClusterTemplate(t, client)
+	clusterTemplate, err := CreateKubernetesClusterTemplate(t, client)
 	th.AssertNoErr(t, err)
 	defer DeleteClusterTemplate(t, client, clusterTemplate.UUID)
 
-	clusterID, err := createKubernetesCluster(t, client, clusterTemplate.UUID)
+	clusterID, err := CreateKubernetesCluster(t, client, clusterTemplate.UUID)
 	th.AssertNoErr(t, err)
 	defer DeleteCluster(t, client, clusterID)
 
@@ -75,107 +70,4 @@ func testNodeGroupGet(t *testing.T, client *gophercloud.ServiceClient, clusterID
 	// Should have got the same node group as from the list
 	th.AssertEquals(t, ngID, ng.UUID)
 	th.AssertEquals(t, "worker", ng.Role)
-}
-
-func createKubernetesClusterTemplate(t *testing.T, client *gophercloud.ServiceClient) (*clustertemplates.ClusterTemplate, error) {
-	choices, err := clients.AcceptanceTestChoicesFromEnv()
-	if err != nil {
-		return nil, err
-	}
-
-	name := tools.RandomString("TESTACC-", 8)
-	t.Logf("Attempting to create cluster template: %s", name)
-
-	boolFalse := false
-	createOpts := clustertemplates.CreateOpts{
-		COE:                 "kubernetes",
-		DNSNameServer:       "8.8.8.8",
-		DockerStorageDriver: "overlay2",
-		ExternalNetworkID:   choices.ExternalNetworkID,
-		FlavorID:            choices.FlavorID,
-		FloatingIPEnabled:   &boolFalse,
-		ImageID:             choices.MagnumImageID,
-		MasterFlavorID:      choices.FlavorID,
-		MasterLBEnabled:     &boolFalse,
-		Name:                name,
-		Public:              &boolFalse,
-		RegistryEnabled:     &boolFalse,
-		ServerType:          "vm",
-	}
-
-	res := clustertemplates.Create(client, createOpts)
-	clusterTemplate, err := res.Extract()
-	if err != nil {
-		return nil, err
-	}
-
-	t.Logf("Successfully created cluster template: %s", clusterTemplate.Name)
-
-	tools.PrintResource(t, clusterTemplate)
-	tools.PrintResource(t, clusterTemplate.CreatedAt)
-
-	th.AssertEquals(t, name, clusterTemplate.Name)
-	th.AssertEquals(t, choices.ExternalNetworkID, clusterTemplate.ExternalNetworkID)
-	th.AssertEquals(t, choices.MagnumImageID, clusterTemplate.ImageID)
-
-	return clusterTemplate, nil
-}
-
-func createKubernetesCluster(t *testing.T, client *gophercloud.ServiceClient, clusterTemplateID string) (string, error) {
-	clusterName := tools.RandomString("TESTACC-", 8)
-	t.Logf("Attempting to create cluster: %s using template %s", clusterName, clusterTemplateID)
-
-	choices, err := clients.AcceptanceTestChoicesFromEnv()
-	if err != nil {
-		return "", err
-	}
-
-	masterCount := 1
-	nodeCount := 1
-	createTimeout := 900
-	createOpts := clusters.CreateOpts{
-		ClusterTemplateID: clusterTemplateID,
-		CreateTimeout:     &createTimeout,
-		FlavorID:          choices.FlavorID,
-		Keypair:           choices.MagnumKeypair,
-		Labels:            map[string]string{},
-		MasterCount:       &masterCount,
-		MasterFlavorID:    choices.FlavorID,
-		Name:              clusterName,
-		NodeCount:         &nodeCount,
-	}
-
-	createResult := clusters.Create(client, createOpts)
-	th.AssertNoErr(t, createResult.Err)
-	if len(createResult.Header["X-Openstack-Request-Id"]) > 0 {
-		t.Logf("Cluster Create Request ID: %s", createResult.Header["X-Openstack-Request-Id"][0])
-	}
-
-	clusterID, err := createResult.Extract()
-	if err != nil {
-		return "", err
-	}
-
-	t.Logf("Cluster created: %+v", clusterID)
-
-	clusterReady := false
-	st := time.Now()
-	for time.Since(st) < 15*time.Minute {
-		time.Sleep(10 * time.Second)
-		cluster, err := clusters.Get(client, clusterID).Extract()
-		if err != nil {
-			return "", fmt.Errorf("Error checking cluster status: %v", err)
-		}
-		if cluster.Status == "CREATE_COMPLETE" {
-			clusterReady = true
-			break
-		}
-	}
-
-	if !clusterReady {
-		return "", fmt.Errorf("Timing out waiting for cluster CREATE_COMPLETE status")
-	}
-
-	t.Logf("Successfully created cluster: %s id: %s", clusterName, clusterID)
-	return clusterID, nil
 }
